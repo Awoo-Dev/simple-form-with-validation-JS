ce9abaa61dd47efe3933e548a8d3c28fb5e10824
Awoo <87096979+Awoo-Dev@users.noreply.github.com>
2023-02-17 18:20:03 -0300

    Add files via upload

---
+++ ejdom.js
@@ -0,0 +1,63 @@
+'use strict'
+
+
+window.addEventListener("load", function(){
+
+console.log("ready")
+
+var form=document.querySelector("#formulario");
+var box_dash=document.querySelector(".dashed");
+
+box_dash.style.display="none"
+form.addEventListener('submit', function(){
+    console.log("catched!");
+
+
+    var nombre= document.querySelector("#name").value;
+    var apellido=document.querySelector("#lastname").value;
+    var edad= parseInt(document.querySelector("#age").value) 
+   
+
+
+    if (nombre.trim()==null || nombre.trim().length ==0) {
+
+        alert("el nombre no es valido");
+        document.getElementById("name").style.border = "2px solid red";
+        return false;
+        
+    }
+
+    if (apellido.trim()==null || apellido.trim().length ==0) {
+
+        alert("el apellido no es valido");
+        document.getElementById("lastname").style.border = "2px solid red";
+        return false;
+        
+    }
+    if (edad==null || edad <=0 || isNaN(edad)) {
+
+        alert("el nombre no es valido");
+        document.getElementById("age").style.border = "2px solid red";
+        return false;
+        
+    }
+
+    
+
+    console.log(nombre, apellido, edad);
+    box_dash.style.display="block"
+
+    var p_nombre=document.querySelector("#p_nombre span");
+    var p_apellido=document.querySelector("#p_apellido span");
+    var p_edad=document.querySelector("#p_edad span");
+
+    p_nombre.innerHTML=nombre;
+    p_apellido.innerHTML=apellido;
+    p_edad.innerHTML=edad;
+
+})
+
+
+
+});
+
